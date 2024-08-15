418bc0ffb244bb3210f2c6aa46b085fb8fdc1856
Yehya <yehyaelzaeem7@gmail.com>
2024-08-15 13:36:25 +0300

    push ios 14 /8

--- lib/Ui/auth_screen/page/signin_page.dart
+++ lib/Ui/auth_screen/page/signin_page.dart
@@ -48,7 +48,8 @@ class _SignInPageState extends State<SignInPage> {
 
   final _bloc = sl<AuthBloc>();
   PhoneNumber number = PhoneNumber(isoCode: 'EG');
-
+  String numberPhone ='';
+  TextEditingController controller =TextEditingController();
   @override
   Widget build(BuildContext context) {
     DateTime date = DateTime(2024, 8, 8);//تاريخ رddddفع اخر نسخه android & ios
@@ -121,13 +122,16 @@ class _SignInPageState extends State<SignInPage> {
                                   padding:  EdgeInsets.only(left: 20.0),
                                   child: Center(
                                     child: InternationalPhoneNumberInput(
+
                                       countries:
                                       [
                                         'AF', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG', 'AR', 'AM', 'AW', 'AU', 'AT', 'AZ', 'BS', 'BH', 'BD', 'BB', 'BY', 'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BQ', 'BA', 'BW', 'BV', 'BR', 'IO', 'BN', 'BG', 'BF', 'BI', 'KH', 'CM', 'CA', 'CV', 'KY', 'CF', 'TD', 'CL', 'CN', 'CX', 'CC', 'CO', 'KM', 'CG', 'CD', 'CK', 'CR', 'HR', 'CU', 'CW', 'CY', 'CZ', 'DK', 'DJ', 'DM', 'DO', 'EC', 'EG', 'SV', 'GQ', 'ER', 'EE', 'SZ', 'ET', 'FK', 'FO', 'FJ', 'FI', 'FR', 'GF', 'PF', 'TF', 'GA', 'GM', 'GE', 'DE', 'GH', 'GI', 'GR', 'GL', 'GD', 'GP', 'GU', 'GT', 'GG', 'GN', 'GW', 'GY', 'HT', 'HM', 'HN', 'HK', 'HU', 'IS', 'IN', 'ID', 'IR', 'IQ', 'IE', 'IM', 'IT', 'CI', 'JM', 'JP', 'JE', 'JO', 'KZ', 'KE', 'KI', 'KP', 'KR', 'KW', 'KG', 'LA', 'LV', 'LB', 'LS', 'LR', 'LY', 'LI', 'LT', 'LU', 'MO', 'MK', 'MG', 'MW', 'MY', 'MV', 'ML', 'MT', 'MH', 'MQ', 'MR', 'MU', 'YT', 'MX', 'FM', 'MD', 'MC', 'MN', 'ME', 'MS', 'MA', 'MZ', 'MM', 'NA', 'NR', 'NP', 'NL', 'NC', 'NZ', 'NI', 'NE', 'NG', 'NU', 'NF', 'MP', 'NO', 'OM', 'PK', 'PW', 'PS', 'PA', 'PG', 'PY', 'PE', 'PH', 'PN', 'PL', 'PT', 'PR', 'QA', 'RE', 'RO', 'RU', 'RW', 'BL', 'SH', 'KN', 'LC', 'MF', 'PM', 'VC', 'WS', 'SM', 'ST', 'SA', 'SN', 'RS', 'SC', 'SL', 'SG', 'SX', 'SK', 'SI', 'SB', 'SO', 'ZA', 'GS', 'SS', 'ES', 'LK', 'SD', 'SR', 'SJ', 'SE', 'CH', 'SY', 'TW', 'TJ', 'TZ', 'TH', 'TL', 'TG', 'TK', 'TO', 'TT', 'TN', 'TR', 'TM', 'TC', 'TV', 'UG', 'UA', 'AE', 'GB', 'US', 'UM', 'UY', 'UZ', 'VU', 'VA', 'VE', 'VN', 'VG', 'VI', 'WF', 'EH', 'YE', 'ZM', 'ZW'
                                       ],
                                       onInputChanged: (PhoneNumber number) {
                                        this.number = number;
-                                      },
+                                       numberPhone=number.parseNumber();
+                                       print('sddsf ${numberPhone}');
+                                       },
                                       maxLength: 100,
                                       onInputValidated: (bool value) {
                                         print(value);
@@ -290,15 +294,10 @@ class _SignInPageState extends State<SignInPage> {
                                       setState(() {
                                         textemail = v = textpasswordlogin;
                                         if (_mailControllerlogin.text.isEmpty) {
-                                          textemail =
-                                              "phone required".tr(context);
-                                        } else if (_passwordControllerlogin
-                                            .text.isEmpty) {
+                                          textemail = "phone required".tr(context);
+                                        } else if (_passwordControllerlogin.text.isEmpty) {
                                           v = "red";
-                                          textpasswordlogin =
-                                              AppLocalizations.of(context)
-                                                  .translate(
-                                                      "password required");
+                                          textpasswordlogin = AppLocalizations.of(context).translate("password required");
                                         }
                                         // else if (!RegExp(
                                         //         r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
@@ -309,7 +308,7 @@ class _SignInPageState extends State<SignInPage> {
                                         // }
                                         else {
                                           _bloc.add(TryLogin((b) => b
-                                            ..phone = number.phoneNumber
+                                            ..phone = _mailControllerlogin.text
                                             ..password =
                                                 _passwordControllerlogin.text));
                                         }
